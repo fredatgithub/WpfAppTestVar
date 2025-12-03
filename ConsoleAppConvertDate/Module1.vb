9ac8c2516ae807da7c8761068fd09e6d0a990e13
Freddy JUHEL <PFJL06651@commun.ad.sncf.fr>
2025-12-03 14:54:39 +0100

    add console app project

---
+++ ConsoleAppConvertDate/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+  Sub Main()
+    Dim prj_date_deb_from_db As String = "03-01-2000"
+    Dim prj_date_deb_as_date As Date
+    Dim allDateAreOk As Boolean = True
+    Dim finalDate As Date
+    Dim formatDate() As String = {"dd/MM/yyyy", "dd-MM-yyyy"}
+    If prj_date_deb_from_db = "unknown" Or prj_date_deb_from_db.StartsWith("ko") Then
+      allDateAreOk = False
+    Else
+      If Date.TryParseExact(prj_date_deb_from_db, formatDate, Globalization.CultureInfo.GetCultureInfo("fr-FR"), Globalization.DateTimeStyles.AssumeLocal, prj_date_deb_as_date) Then
+        finalDate = prj_date_deb_as_date
+      Else
+        allDateAreOk = False
+      End If
+    End If
+
+    Console.WriteLine($"La date finale est : {finalDate.ToShortDateString()}")
+    Console.WriteLine("Appuyez sur une touche pour continuer...")
+    Console.ReadKey()
+  End Sub
+End Module
---
+++ ConsoleAppConvertDate/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("ConsoleAppConvertDate")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleAppConvertDate")>
+<Assembly: AssemblyCopyright("Copyright © MIT 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("1227f574-d88f-432b-bd4e-afb40556b843")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
